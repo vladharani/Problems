a369fdbeb368387e418985545e27a6fca5baae56
Vlad Harani <vlad.harani@live.com>
2016-01-05 19:08:59 +0200

    Problema 1 - Sportiv

---
+++ ProblemOne_Sportiv/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemOne_Sportiv
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestNumberOfRepetitions()
+        {
+            int x = 4;
+            int numberOfRepetitions = x * x;
+            Assert.AreEqual(16, numberOfRepetitions);
+        }
+    }
+}
